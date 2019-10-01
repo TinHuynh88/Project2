c9b686d145e3ea17a41eaa7875e8bc38416c76d2
coopersamjohn <coopersamjohn@gmail.com>
2019-10-01 11:39:59 -0500

    blah

--- Project2_Angular/src/app/product.spec.ts
+++ Project2_Angular/src/app/product.spec.ts
@@ -4,7 +4,7 @@ describe('Products', () => {
   it('should create an instance', () => {
     const x = new Date();
     expect(new Product(999, 'Video Game', 99, '2000/01/01',
-      'Platformer', 10, 5, 'SNES', 
+      'Platformer', 10, 5, 'SNES',
       'https://content.propertyroom.com/listings/sellers/seller888888939/images/origimgs/888888939_183201702445865.jpg')).toBeTruthy();
   });
 });
