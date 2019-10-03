a7dcfcdb8192488bdc7234ba1114fa191843d870
unknown <thienuy2009@gmail.com>
2019-10-03 13:22:22 -0500

    Updating Products to be Product due to team's decision

--- Project2_Angular/src/app/admin-create-product/admin-create-product.component.ts
+++ Project2_Angular/src/app/admin-create-product/admin-create-product.component.ts
@@ -1,22 +1,5 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-admin-create-product',
-  templateUrl: './admin-create-product.component.html',
-  styleUrls: ['./admin-create-product.component.css']
-})
-export class AdminCreateProductComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
-=======
-import { Component, OnInit } from '@angular/core';
-import { Products } from '../products';
+import { Product } from '../product';
 import { ProductsService } from '../products.service';
 import { Router } from '@angular/router';
 
@@ -26,9 +9,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./admin-create-product.component.css']
 })
 export class AdminCreateProductComponent {
-  product:Products;
+  product:Product;
   constructor(private productService:ProductsService, private router:Router) {
-    this.product=new Products();
+    //this.product=new Products();
   }
   createProduct() {
     this.productService.createProduct(this.product).subscribe(data=>this.gotoProductList());
@@ -39,4 +22,3 @@ export class AdminCreateProductComponent {
   }
 }
 
->>>>>>> 08fba8f654623e68484b57814a5345a36c0b3dba
