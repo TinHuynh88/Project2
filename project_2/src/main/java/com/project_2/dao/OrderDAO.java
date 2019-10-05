4ac60d6f3db20ee4ab341728d69e5b9fa6cd1db8
THANH TIN HUYNH <thanhtinphuynh@gmail.com>
2019-10-05 16:52:27 -0500

    Add order table name

--- project_2/src/main/java/com/project_2/dao/OrderDAO.java
+++ project_2/src/main/java/com/project_2/dao/OrderDAO.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.project_2.model.Order;
-import com.project_2.model.Products;
 
 @Repository
 public interface OrderDAO extends JpaRepository<Order, Long> {
