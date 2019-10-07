f337e1783f39de108c34c9fdf8c568e3177323a3
viral <mviralk@gmail.com>
2019-10-06 21:34:42 -0500

    Login and Register works

--- project_2/src/main/java/com/project_2/dao/TransactionDAO.java
+++
@@ -1,9 +0,0 @@
-package com.project_2.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project_2.model.Transaction;
-
-public interface TransactionDAO extends JpaRepository<Transaction, Long>{
-
-}
